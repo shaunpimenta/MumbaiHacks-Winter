35e323fcb3396ef68f0a5bab6ab011b5ce029901
Atharva Prashant Pawar <67649944+AtharvaPawar456@users.noreply.github.com>
2023-12-17 01:10:49 +0530

    v1

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,57 +1,19 @@
-# pip install Flask Flask-CORS
-
-
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    try:
-        data = request.json  # Assuming the incoming data is in JSON format
-        # print("Received data:", data)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-        print("Received User data:", data['user'])
-        print("Received Message data:", data['message'])
+@app.route('/chat/<input_chat>')
+def chat(input_chat):
+    # Extract color from the chat input (assuming the input is in the format "#RRGGBB")
+    color_hex = input_chat.split(' ')[-1]
+    color = int(color_hex[1:], 16)  # Convert hex to decimal
 
-        colorPalate = ["orange", "dark orange", "blue", "red", "white"]
-
-        for index, item in enumerate(colorPalate):
-            if item in data['message']:
-                selectedcolor = index
-
-        # Add your processing logic here if needed
-
-        response_data = {
-            "message": "Data received successfully",
-            "color_select": [selectedcolor, 0, 0, 0]  # body color, mirror, wheel, caliber
-                         }
-        return jsonify(response_data)
-
-    except Exception as e:
-        error_message = {"error": str(e)}
-        return jsonify(error_message), 400  # Return a 400 status code for bad requests
+    # Call the JavaScript function to change the material color
+    return f'<script>changeMaterialColor({color});</script>'
 
 if __name__ == '__main__':
     app.run(debug=True)
-'''
-Received data: {'user': 'John Doe', 'message': 'Hello, Flask API!'}
-
-["orange", "dark orange", "blue", "red", "white"]
-
-0:orange
-1:dark orange
-2:blue
-3:red
-4:white
-
-0:orange
-1:dark orange
-2:blue
-3:red
-4:white
-
-body, mirror, wheel, caliber
-'''

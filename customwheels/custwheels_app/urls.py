e325b518f63094adc211eee46a48e18d80717961
Atharva Prashant Pawar <67649944+AtharvaPawar456@users.noreply.github.com>
2023-12-16 17:47:53 +0530

    integrated the html page and login controls

--- customwheels/custwheels_app/urls.py
+++ customwheels/custwheels_app/urls.py
@@ -3,7 +3,16 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    
+    path("select_model/", views.select_model, name="select_model"),
+
+    path('chat/', views.chat_view, name='chat_view'),
+
+
+    path("customemodel", views.customemodel, name="customemodel"),
+
+    path("test", views.test, name="test"),
+    path("help", views.help, name="help"),
+
     # path("business", views.business, name="Business"),
     # path("about/", views.about, name="AboutUs"),
     # path("contact/", views.contact, name="ContactUs"),
--- customwheels/custwheels_app/views.py
+++ customwheels/custwheels_app/views.py
@@ -1,8 +1,20 @@
 #  i have created this file - GTA
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout, logout
+
+from django.contrib.auth.decorators import login_required
+
+
+
 # from django.http import HttpResponse
 # from .models import Product, Contact
 # import random
+
+chatList = ["hello"]
 
 
 # Create your views here.
@@ -10,6 +22,77 @@ def index(request):
     # return HttpResponse('Teamzeffort    |      business Page')
     return render(request,'custwheels_app/welcome.html')
 
+def select_model(request):
+    return render(request,'custwheels_app/select_model.html')
+
+def customemodel(request):
+    return render(request,'custwheels_app/customemodel.html')
+
+
+def test(request):
+    return render(request,'custwheels_app/loadobjModel.html')
+
+def help(request):
+    return render(request,'custwheels_app/help.html')
+
+
+@csrf_exempt  # To disable CSRF protection (not recommended for production)
+def chat_view(request):
+    if request.method == 'POST':
+        input_text = request.POST.get('input_text', '')  # Get the input field value
+        print(f"Received input: {input_text}")  # Print the input in the terminal
+          
+        chatList.append(input_text)
+        # params = {"chatList" : input_text}
+        params = {
+            "chatList" : chatList,
+            # "light_diffuse" : light_diffuse
+            }
+    return render(request,'custwheels_app/customemodel.html', params)
+
+
+
+
+def user_register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        
+        # Check if the username is unique
+        if not User.objects.filter(username=username).exists():
+            # Create a new user
+            user = User.objects.create_user(username=username, password=password, email=email)
+            return redirect('user_login')  # Redirect to your login view
+        else:
+            error_message = 'Username already exists'
+    else:
+        error_message = None
+
+    return render(request, 'agroapp/register.html', {'error_message': error_message})
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('viewNodes')  # Redirect to your dashboard view
+        else:
+            error_message = 'Invalid username or password'
+    else:
+        error_message = None
+
+    return render(request, 'agroapp/login.html', {'error_message': error_message})
+
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('user_login')  # Redirect to your login view
 
 # ------------------------------- Extra ------------------------------- 
 

b96533d17e9475657e0a9e8cbf25878d49d20c25
Atharva Prashant Pawar <67649944+AtharvaPawar456@users.noreply.github.com>
2023-12-17 05:41:11 +0530

    chat view

--- customwheels/custwheels_app/views.py
+++ customwheels/custwheels_app/views.py
@@ -2,27 +2,39 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout, logout
 
 from django.contrib.auth.decorators import login_required
 
 
+# NER 
+import spacy
+nlp = spacy.load("en_core_web_sm")# Load the spaCy model with NER component
+
+import re
+# import nltk
+# from nltk.stem import PorterStemmer
+
 
 # from django.http import HttpResponse
 # from .models import Product, Contact
 # import random
 
-chatList = ["hello"]
+chatList = []
 
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Teamzeffort    |      business Page')
+    global chatList
+    chatList = []
     return render(request,'custwheels_app/welcome.html')
 
 def select_model(request):
+    global chatList
+    chatList = []
     return render(request,'custwheels_app/select_model.html')
 
 
@@ -51,14 +63,24 @@ def test(request):
 # def help(request):
 #     return render(request,'custwheels_app/help.html')
 
-
+@login_required
 @csrf_exempt  # To disable CSRF protection (not recommended for production)
 def chat_view(request):
     if request.method == 'POST':
         input_text = request.POST.get('input_text', '')  # Get the input field value
         print(f"Received input: {input_text}")  # Print the input in the terminal
-          
+
+        if input_text == "-h" or input_text == "help" or input_text == "--help":
+            print("Help: Your help message here")
+            __Reply = help_info()
+        
+        else:
+            __Reply = filtertext(input_text)
+
+        global chatList
         chatList.append(input_text)
+        chatList.append(__Reply)
+
         # params = {"chatList" : input_text}
         params = {
             "chatList" : chatList,
@@ -66,6 +88,183 @@ def chat_view(request):
             }
     return render(request,'custwheels_app/customemodel.html', params)
 
+
+# ---------------------------------------------------------------------------
+# Custom Funtions
+# ---------------------------------------------------------------------------
+
+def help_info():
+    value = """
+        Display help information for types of materials and available colors.<br>
+
+        Types of Materials:<br>
+        - tire: "__type1", "__type2"<br>
+        - tire_color: "black", "white", "gray"<br>
+        - bonnet: "__type1"<br>
+        - spoiler: "__type1"<br>
+
+        Available Colors:<br>
+        "red", "black", "blue", "green", "white",<br>
+        "silver", "yellow", "purple", "orange",<br>
+        "gray", "pink", "brown", "turquoise",<br> 
+        "gold", "maroon", "lavender", "teal", "cyan",<br>
+        "indigo", "violet"<br>
+    """
+    return value
+
+# red, black, blue, green, white, silver, yellow, purple, orange, gray, pink, brown, turquoise, gold, maroon, lavender, teal, cyan, indigo, violet
+
+typesMaterials = ["tire", "bonnet", "spoiler"]
+
+typesMaterials = {
+    "tire": ["__type1", "__type2"],
+    "tire_color": ["black", "white", "gray"],
+
+    "bonnet": ["__type1"],
+    
+    "spoiler": ["__type1"],
+
+    # "bonnet": "spoiler"
+    }
+
+part_names = ["wheels", "wheel", "tires", "tire", "light", "exhaust","glass", "dashboard", "body", "spoiler", "spoilers", "wing", "lighting", "lights"]
+
+colors = ["red", "black", "blue", "green", "white", "silver", "yellow", "purple", "orange", "gray", "pink", "brown", "turquoise", "gold", "maroon", "lavender", "teal", "cyan", "indigo", "violet"]
+
+
+def filtertext(input_text):
+    # Function to remove stop words from a chat
+    def remove_stop_words(chat):
+        doc = nlp(chat)
+        filtered_words = [token.text for token in doc if not token.is_stop]
+        return " ".join(filtered_words)
+
+    # Function to perform lemmatization on a chat
+    def lemmatize_chat(chat):
+        doc = nlp(chat)
+        lemmatized_words = [token.lemma_ for token in doc]
+        return " ".join(lemmatized_words)
+
+    # Function to make every word in a chat lowercase
+    def make_chat_lowercase(chat):
+        return chat.lower()
+
+    # Function to remove symbols from a chat
+    def remove_symbols(chat):
+        chat_without_symbols = re.sub(r'[,.\'";?@-]', '', chat)
+        return chat_without_symbols
+    
+    def checkActions(processed_text):
+        for item in part_names:
+            if item in processed_text:
+                __part_name = True
+            else:
+                __part_name = False
+        for item in colors:
+            if item in processed_text:
+                __color_name = True
+            else:
+                __color_name = False
+        retValue = {
+            "__color_name" : __color_name,
+            "__part_name" : __part_name,
+        }
+        
+        return retValue
+
+
+    # input_text = "I'm thinking about customizing my car with black Wheels."
+
+    # Apply all text processing steps to the input_text
+    processed_text = remove_stop_words(input_text)
+    processed_text = lemmatize_chat(processed_text)
+    processed_text = make_chat_lowercase(processed_text)
+    processed_text = remove_symbols(processed_text)
+
+    checks_value = checkActions(processed_text)
+
+    # Extract color and part name from the input_text
+    colors_found, parts_found = extract_color_and_part_from_text(processed_text, checks_value) 
+
+    if "none" != colors_found:
+        color_value = map_color_to_mtl(colors_found)
+    else:
+        color_value = "none"
+
+    data = {
+        "Input_Text" : input_text,
+
+        "__part_name_check" : checks_value['__part_name'],
+        "__color_name_check" : checks_value['__color_name'],
+        
+        "Part_Names" : parts_found,
+        "Colors" : colors_found,
+
+        "color_value" : color_value,
+    }
+    # print(f"Input Text: {input_text} \t\t\t Colors: {colors_found} \t\t\t Part Names: {parts_found}")
+    return data
+
+
+
+def extract_color_and_part_from_text(input_text, checks_value):
+
+    if checks_value['__part_name']:
+        part_matches = re.findall(r'\b(?:' + '|'.join(part_names) + r')\b', input_text, flags=re.IGNORECASE)
+        __part_matches = part_matches[0] if part_matches else None
+
+    else:
+        __part_matches = 'none'
+
+    if checks_value['__color_name']:
+        color_matches = re.findall(r'\b(?:' + '|'.join(colors) + r')\b', input_text, flags=re.IGNORECASE)
+        __color_matches = color_matches[0] if color_matches else None
+
+    else:
+        __color_matches = 'none'
+    
+    return __color_matches, __part_matches
+
+
+def map_color_to_mtl(color_name):
+    # Define the list of colors and their corresponding RGB values
+    colors = {
+        "red": [1.0, 0.0, 0.0, 1.0],
+        "black": [0.0, 0.0, 0.0, 1.0],
+        "blue": [0.0, 0.0, 1.0, 1.0],
+        "green": [0.0, 1.0, 0.0, 1.0],
+        "white": [1.0, 1.0, 1.0, 1.0],
+        "silver": [0.75, 0.75, 0.75, 1.0],
+        "yellow": [1.0, 1.0, 0.0, 1.0],
+        "purple": [0.5, 0.0, 0.5, 1.0],
+        "orange": [1.0, 0.65, 0.0, 1.0],
+        "gray": [0.5, 0.5, 0.5, 1.0],
+        "pink": [1.0, 0.75, 0.8, 1.0],
+        "brown": [0.65, 0.16, 0.16, 1.0],
+        "turquoise": [0.25, 0.88, 0.82, 1.0],
+        "gold": [1.0, 0.84, 0.0, 1.0],
+        "maroon": [0.5, 0.0, 0.0, 1.0],
+        "lavender": [0.71, 0.49, 0.86, 1.0],
+        "teal": [0.0, 0.5, 0.5, 1.0],
+        "cyan": [0.0, 1.0, 1.0, 1.0],
+        "indigo": [0.29, 0.0, 0.51, 1.0],
+        "violet": [0.93, 0.51, 0.93, 1.0]
+    }
+
+    if color_name in colors:
+        color_values = colors[color_name]
+        print("mtl - color_name : ", color_name)
+        return color_values
+    else:
+        default = [0.0, 0.0, 0.0, 1.0]
+        return default
+
+'''
+Material List:
+
+["Mt_Abs_Black_Gloss", "Mt_ABS_Black_Mat", "Mt_AlloyWheels", "Mt_AventadorAtlas", "Mt_Body", "Mt_BrakeCaliper", "Mt_Chrome", "Mt_Glass_Translucent", "Mt_Interior_Black", "Mt_Light_Emissive", "Mt_Metal_Black_Glossy", "Mt_Metal_Brushed", "Mt_Mirror", "Mt_MirrorCover", "Mt_Reflector_BL", "Mt_Reflector_RL", "Mt_Reflector_TL", "Mt_Reflector_RL", "Mt_TurnLights", "Mt_Tyres", "Mt_WindScreens", "skyBox"]
+
+'''
 
 # ------------------------------ Login Controls ------------------------------ 
 
@@ -109,6 +308,22 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('user_login')  # Redirect to your login view
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # ------------------------------- Extra ------------------------------- 
 
---
+++ customwheels/test.py
@@ -0,0 +1,28 @@
+import re
+
+def find_part_match(input_text, part_names):
+    """
+    Find the first match for part names in the input text.
+
+    Parameters:
+    - input_text (str): The input text to search.
+    - part_names (list): List of part names to search for.
+
+    Returns:
+    - str or None: The first matched part name or None if no match is found.
+    """
+    # Create a regular expression pattern using part names
+    pattern = r'\b(?:' + '|'.join(part_names) + r')\b'
+
+    # Search for the first match
+    match = re.search(pattern, input_text, flags=re.IGNORECASE)
+
+    # Return the matched part name or None
+    return match.group(0) if match else None
+
+# Example usage:
+input_text = "I have a tire and a spoiler on my car."
+part_names = ["tire", "spoiler", "bonnet"]
+
+result = find_part_match(input_text, part_names)
+print(result)

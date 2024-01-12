9c05e10d7660151c8b0fa11c9512381d74331b2d
Atharva Prashant Pawar <67649944+AtharvaPawar456@users.noreply.github.com>
2024-01-12 20:54:42 +0530

    v1

--- flask_server/old_files/test.py
+++ flask_server/old_files/test.py
@@ -7,7 +7,8 @@ import json
 # http://127.0.0.1:5000
 
 # server = "http://127.0.0.1:5000/"
-server = "https://customwheelsapi.atharvapawar.repl.co/"
+# server = "https://customwheelsapi.atharvapawar.repl.co/"
+server = "https://ffb9-35-185-185-55.ngrok-free.app/"
 api_url = f'{server}chat'
 
 # Sample data to send in the POST request
@@ -22,17 +23,23 @@ json_data = json.dumps(data)
 # Set the headers to indicate that you are sending JSON data
 headers = {'Content-Type': 'application/json'}
 
-# Send the POST request
-response = requests.post(api_url, data=json_data, headers=headers)
+try:
+    # Send the POST request
+    response = requests.post(api_url, data=json_data, headers=headers)
+    response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
 
-# Print the response
-print("Response Code:", response.status_code)
-print("Response JSON:", response.json())
+    # Print the response
+    print("Response Code:", response.status_code)
+    print("Response JSON:", response.json())
+
+except requests.exceptions.RequestException as e:
+    print(f"Error in request: {e}")
 
 
 '''
 chats examples:
 
+curl -X POST -d "message=Hello" https://ccc7-35-185-185-55.ngrok-free.app/chat
 
 
 '''
